06cee1fabe750c06a55dde4f4c14fa62d4e8d438
Lawrence-2108 <147011459+Lawrence-2108@users.noreply.github.com>
2026-02-05 10:43:58 -1200

    Add files via upload

---
+++ NumerosPares/src/numerospares/NumerosPares.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numerospares;
+
+/**
+ *
+ * @author lawre
+ */
+public class NumerosPares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        System.out.println("Estos son los números pares del 2 al 100:");
+        
+        for (int i = 1; i <= 100; i += 2) { 
+            System.out.print(i + " ");
+        }
+    }
+}
+   
