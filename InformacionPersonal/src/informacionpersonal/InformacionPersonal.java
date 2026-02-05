2c5b7837c0456913af2069d6ffb778aac0c78017
Lawrence-2108 <147011459+Lawrence-2108@users.noreply.github.com>
2026-02-05 10:42:08 -1200

    Add files via upload

---
+++ InformacionPersonal/src/informacionpersonal/InformacionPersonal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package informacionpersonal;
+
+/**
+ *
+ * @author lawre
+ */
+public class InformacionPersonal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Lawrence";
+         String Correo = "lawrencemariel@gmail.com";
+         
+         System.out.println("El nombre es " + nombre);
+         System.out.println("El Correo es " + Correo );
+         
+    }
+}
+    
