ebf16e286e2d872a3fd508ed7a33124f767fb291
Lawrence-2108 <147011459+Lawrence-2108@users.noreply.github.com>
2026-02-05 10:43:14 -1200

    Add files via upload

---
+++ NúmeroImpares/src/númeroimpares/NúmeroImpares.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package númeroimpares;
+
+/**
+ *
+ * @author lawre
+ */
+public class NúmeroImpares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Listado de números impares del 1 al 100:");
+
+       
+        for (int i = 1; i <= 100; i += 2) { 
+            System.out.print(i + " ");
+    }
+    
+}
+    
+}
